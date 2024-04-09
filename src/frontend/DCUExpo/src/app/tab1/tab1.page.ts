bd62fdef4199ad6525b0026564b7004149dd4f45
Matt Simon Enriquez <matt.enriquez3@mail.dcu.ie>
2024-04-09 21:39:27 +0100

    Implemented filtering by project area and technology

--- src/frontend/DCUExpo/src/app/tab1/tab1.page.ts
+++ src/frontend/DCUExpo/src/app/tab1/tab1.page.ts
@@ -3,6 +3,9 @@ import { ApiHttpService } from '../services/api-http.service';
 import { Project } from '../models/project';
 import { ProjectService } from '../services/project.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { technologies } from '../models/projectTechnologyList';
+import { areas } from '../models/projectAreaList';
 
 @Component({
   selector: 'app-tab1',
@@ -12,6 +15,9 @@ import { Router } from '@angular/router';
 export class Tab1Page {
 
   data : any;
+  dataLoaded: boolean = false;
+  technologiesList : any = technologies;
+  areasList : any = areas;
   searchString : string = "";
   private _apiHttpService : ApiHttpService
   private _projectService : ProjectService
@@ -31,18 +37,41 @@ export class Tab1Page {
   // }
 
   ionViewWillEnter() {
-    this.getProject();
+    //this.getProject();
   }
 
-  selectProject(item: Project){
+  selectProject(item: Project) {
     console.log(item);
     this._projectService.assignProjectId(item.projectId);
     this.router.navigateByUrl('/tabs/tab3');
   }
 
   getProject() {
+    this.dataLoaded = false;
     var url = "https://localhost:7025/api/ExpoProject"
     this._apiHttpService.get<Project>(url).subscribe((response: any) => {
+      this.dataLoaded = true;
+      this.data = response
+      console.log(response);
+    })
+  }
+
+  getProjectByTechnology(technology: string) {
+    console.log(technology);
+    this.dataLoaded = false;
+    var url = "https://localhost:7025/api/ExpoProject/projecttechnology/" + technology
+    this._apiHttpService.get<Project>(url).subscribe((response: any) => {
+      this.dataLoaded = true;
+      this.data = response
+      console.log(response);
+    })
+  }
+
+  getProjectByArea(area: string) {
+    this.dataLoaded = false;
+    var url = "https://localhost:7025/api/ExpoProject/projectarea/" + area
+    this._apiHttpService.get<Project>(url).subscribe((response: any) => {
+      this.dataLoaded = true;
       this.data = response
       console.log(response);
     })
