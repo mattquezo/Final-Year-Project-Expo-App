6a52332275589ddf2f13f1341c295638edf6713c
Matt Simon Enriquez <matt.enriquez3@mail.dcu.ie>
2024-04-10 18:46:15 +0100

    Added Project Location and Student Programme list to models

---
+++ src/frontend/DCUExpo/src/app/models/projectLocationList.ts
@@ -0,0 +1,9 @@
+export var locations = [
+    "LG25",
+    "LG26",
+    "LG27",
+    "L128",
+    "L125",
+    "L114",
+    "L101"
+]
---
+++ src/frontend/DCUExpo/src/app/models/studentProgrammeList.ts
@@ -0,0 +1,11 @@
+export var programmes = [
+    "Data Science",
+    "Computer Applications",
+    "Enterprise Computing",
+    "Mechanical and Manufacturing Engineering (Year 4)",
+    "Mechanical and Manufacturing Engineering (Year 5)",
+    "Mechatronic Engineering",
+    "Biomedical Engineering (Year 4)",
+    "Biomedical Engineering (Year 5)",
+    "Electronic and Computer Engineering"
+  ];
