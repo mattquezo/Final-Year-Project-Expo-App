62a4624812cf61041f35287fb6857a4fba7cd678
Matt Simon Enriquez <matt.enriquez3@mail.dcu.ie>
2024-04-11 22:32:33 +0100

    Implemented clickable image map to project booths in L101 and L114

--- src/frontend/DCUExpo/src/app/tab2/tab2.page.ts
+++ src/frontend/DCUExpo/src/app/tab2/tab2.page.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { ImageMapCoordinate } from '../image-map/image-map.component';
+import { ApiHttpService } from '../services/api-http.service';
+import { ProjectService } from '../services/project.service';
+import { Router } from '@angular/router';
+import { Project } from '../models/project';
+import { coordinatesL101,coordinatesL125,coordinatesL114,coordinatesL128, 
+  coordinatesLG25, coordinatesLG26, coordinatesLG27 } from '../data/imagedata';
+import { MapService } from '../services/map.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +16,66 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  image: string = '../assets/rooms/L101.png'
+  coordinates: ImageMapCoordinate[] = coordinatesL101;
+
+  selectedMap: string = "None";
+  mode: string = "Map Select";
+
+  showImage: boolean = true;
+  private _apiHttpService: ApiHttpService;
+  private _projectService: ProjectService;
+  private _mapService: MapService;
+
+  getClick(coordinate: ImageMapCoordinate) {
+    console.log(`Clicked on ${coordinate.name}`)
+    this.selectProjectId(coordinate.name!);
+  }
+
+  selectMap(selectedMap: string){
+    this._mapService.setSelectedMap(selectedMap);
+    this.selectedMap = selectedMap;
+    this.mode = "Map";
+    console.log(selectedMap);
+    switch (selectedMap){
+      case "L101":
+        this.coordinates = coordinatesL101;
+        break;
+      case "L114":
+        this.coordinates = coordinatesL114;
+        break;
+      case "L125":
+        this.coordinates = coordinatesL125;
+        break;
+      case "L128":
+        this.coordinates = coordinatesL128;
+        break;
+      case "LG25":
+        this.coordinates = coordinatesLG25;
+        break;
+      case "LG26":
+        this.coordinates = coordinatesLG26;
+        break;
+      case "LG27":
+        this.coordinates = coordinatesLG27;
+        break;
+    }
+
+    console.log(this.coordinates);
+
+  }
+
+  constructor(apiHttpService : ApiHttpService, projectService : ProjectService, mapService: MapService,
+    private router: Router) {
+    this._apiHttpService = apiHttpService
+    this._projectService = projectService
+    this._mapService = mapService
+  }
+
+  selectProjectId(id: string) {
+    console.log(id);
+    this._projectService.assignProjectId(parseInt(id));
+    this.router.navigateByUrl('/tabs/tab3');
+  }
 
 }
