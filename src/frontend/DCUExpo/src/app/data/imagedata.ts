47324c8775cc437290a0b3505df38c3fbeee5110
Matt Simon Enriquez <matt.enriquez3@mail.dcu.ie>
2024-04-11 22:30:02 +0100

    Added necessary data for image mapping such as location maps

---
+++ src/frontend/DCUExpo/src/app/data/imagedata.ts
@@ -0,0 +1,345 @@
+import { ImageMapCoordinate } from "../image-map/image-map.component"
+
+export var coordinatesL101: ImageMapCoordinate[] = [
+  {
+    name: '173',
+    x: 24,
+    y: 2,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '174',
+    x: 50,
+    y: 2,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '175',
+    x: 87,
+    y: 2,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '176',
+    x: 75,
+    y: 12,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '177',
+    x: 50,
+    y: 12,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '178',
+    x: 24.5,
+    y: 12,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '179',
+    x: 12.5,
+    y: 12,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '180',
+    x: 24.5,
+    y: 20.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '181',
+    x: 50,
+    y: 20.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '182',
+    x: 75,
+    y: 20.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '183',
+    x: 87,
+    y: 32,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '184',
+    x: 62.5,
+    y: 32,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '185',
+    x: 36.5,
+    y: 32,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '186',
+    x: 12.5,
+    y: 32,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '187',
+    x: 24.5,
+    y: 40,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '188',
+    x: 50,
+    y: 40,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '189',
+    x: 75,
+    y: 40,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '190',
+    x: 87,
+    y: 52,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '191',
+    x: 62.5,
+    y: 52,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '192',
+    x: 36.5,
+    y: 52,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '193',
+    x: 12.5,
+    y: 52,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '194',
+    x: 24.5,
+    y: 60,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '195',
+    x: 50,
+    y: 60,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '196',
+    x: 75,
+    y: 60,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '197',
+    x: 87,
+    y: 71,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '198',
+    x: 62.5,
+    y: 71,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '199',
+    x: 36.5,
+    y: 71,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '200',
+    x: 12.5,
+    y: 71,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '201',
+    x: 12.5,
+    y: 79.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '202',
+    x: 36.5,
+    y: 79.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '203',
+    x: 62.5,
+    y: 79.5,
+    width: 11,
+    height: 8
+  }
+]
+
+export var coordinatesL114: ImageMapCoordinate[] = [
+  {
+    name: '143',
+    x: 50,
+    y: 11.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '144',
+    x: 12.5,
+    y: 11.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '145',
+    x: 12.5,
+    y: 20,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '146',
+    x: 37,
+    y: 20,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '147',
+    x: 63,
+    y: 20,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '148',
+    x: 87,
+    y: 20,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '149',
+    x: 75,
+    y: 32,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '150',
+    x: 50,
+    y: 32,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '151',
+    x: 24.5,
+    y: 32,
+    width: 11,
+    height: 8
+  }
+]
+
+export var coordinatesL125: ImageMapCoordinate[] = [
+  {
+    name: '142',
+    x: 1,
+    y: 1,
+    width: 11,
+    height: 9
+  },
+  {
+    name: '141',
+    x: 13,
+    y: 1,
+    width: 11,
+    height: 9
+  }
+
+]
+
+export var coordinatesL128: ImageMapCoordinate[] = [
+  {
+    name: '173',
+    x: 24,
+    y: 2,
+    width: 11,
+    height: 8
+  }
+]
+
+export var coordinatesLG25: ImageMapCoordinate[] = [
+  {
+    name: '173',
+    x: 24,
+    y: 2,
+    width: 11,
+    height: 8
+  }
+]
+
+export var coordinatesLG26: ImageMapCoordinate[] = [
+  {
+    name: '173',
+    x: 24,
+    y: 2,
+    width: 11,
+    height: 8
+  }
+]
+
+export var coordinatesLG27: ImageMapCoordinate[] = [
+  {
+    name: '173',
+    x: 24,
+    y: 2,
+    width: 11,
+    height: 8
+  }
+]
