5a3d34151f4ade4f45ccda666e5398d3ea34b7b4
Matt Simon Enriquez <matt.enriquez3@mail.dcu.ie>
2024-04-12 18:54:27 +0100

    Completed image mapping for L114 and L125

--- src/frontend/DCUExpo/src/app/data/imagedata.ts
+++ src/frontend/DCUExpo/src/app/data/imagedata.ts
@@ -283,34 +283,453 @@ export var coordinatesL114: ImageMapCoordinate[] = [
     y: 32,
     width: 11,
     height: 8
+  },
+  {
+    name: '152',
+    x: 12.5,
+    y: 40,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '153',
+    x: 37,
+    y: 40,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '154',
+    x: 63,
+    y: 40,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '155',
+    x: 87,
+    y: 40,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '156',
+    x: 75,
+    y: 51.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '157',
+    x: 50,
+    y: 51.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '158',
+    x: 24.5,
+    y: 51.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '159',
+    x: 12.5,
+    y: 60,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '160',
+    x: 37,
+    y: 60,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '161',
+    x: 63,
+    y: 60,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '162',
+    x: 87,
+    y: 60,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '163',
+    x: 75,
+    y: 70.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '164',
+    x: 50,
+    y: 70.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '165',
+    x: 24.5,
+    y: 70.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '166',
+    x: 12.5,
+    y: 79,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '167',
+    x: 37,
+    y: 79,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '168',
+    x: 63,
+    y: 79,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '169',
+    x: 87,
+    y: 79,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '170',
+    x: 87,
+    y: 90,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '171',
+    x: 63,
+    y: 90,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '172',
+    x: 37,
+    y: 90,
+    width: 11,
+    height: 8
   }
 ]
 
 export var coordinatesL125: ImageMapCoordinate[] = [
   {
+    name: '102',
+    x: 1,
+    y: 89,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '103',
+    x: 22.5,
+    y: 89,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '104',
+    x: 32.5,
+    y: 73,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '105',
+    x: 22.5,
+    y: 73,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '106',
+    x: 1,
+    y: 73,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '107',
+    x: 1,
+    y: 65,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '108',
+    x: 12,
+    y: 65,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '109',
+    x: 32.5,
+    y: 65,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '110',
+    x: 57,
+    y: 65,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '111',
+    x: 78,
+    y: 65,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '112',
+    x: 88.5,
+    y: 52.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '113',
+    x: 68,
+    y: 52.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '114',
+    x: 57,
+    y: 52.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '115',
+    x: 32.5,
+    y: 52.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '116',
+    x: 22.5,
+    y: 52.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '117',
+    x: 1,
+    y: 52.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '118',
+    x: 1,
+    y: 44.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '119',
+    x: 12,
+    y: 44.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '120',
+    x: 32.5,
+    y: 44.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '121',
+    x: 57,
+    y: 44.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '122',
+    x: 78,
+    y: 44.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '123',
+    x: 88.5,
+    y: 44.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '124',
+    x: 88.5,
+    y: 31.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '125',
+    x: 68,
+    y: 31.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '126',
+    x: 57,
+    y: 31.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '127',
+    x: 32.5,
+    y: 31.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '128',
+    x: 12,
+    y: 31.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '129',
+    x: 1,
+    y: 31.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '130',
+    x: 1,
+    y: 23.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '131',
+    x: 22.5,
+    y: 23.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '132',
+    x: 32.5,
+    y: 23.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '133',
+    x: 57,
+    y: 23.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '134',
+    x: 78,
+    y: 23.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '135',
+    x: 88.5,
+    y: 23.5,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '136',
+    x: 88.5,
+    y: 1,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '137',
+    x: 68,
+    y: 1,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '138',
+    x: 57,
+    y: 1,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '139',
+    x: 46,
+    y: 1,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '140',
+    x: 22.5,
+    y: 1,
+    width: 11,
+    height: 8
+  },
+  {
+    name: '141',
+    x: 12,
+    y: 1,
+    width: 11,
+    height: 8
+  },
+  {
     name: '142',
     x: 1,
     y: 1,
     width: 11,
-    height: 9
+    height: 8
   },
-  {
-    name: '141',
-    x: 13,
-    y: 1,
-    width: 11,
-    height: 9
-  }
-
 ]
 
 export var coordinatesL128: ImageMapCoordinate[] = [
   {
-    name: '173',
-    x: 24,
+    name: '71',
+    x: 4,
     y: 2,
     width: 11,
-    height: 8
+    height: 10
   }
 ]
 
